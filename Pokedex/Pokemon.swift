4142c0801b9a27f09c26237a0b324bb3bce4c658
anashmamouch <anashmamouch90@gmail.com>
2016-02-29 11:32:52 +0000

    added Model class Pokemon

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Anas Hmamouch on 29/2/16.
+//  Copyright © 2016 Benzino. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon{
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var  name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
