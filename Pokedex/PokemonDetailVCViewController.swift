0af0b63576b2fa07d5a4a01de65189710cf9d663
anashmamouch <anashmamouch90@gmail.com>
2016-03-01 17:45:36 +0000

    connected IBOutlets to DetailsViewController class

--- Pokedex/PokemonDetailVCViewController.swift
+++ Pokedex/PokemonDetailVCViewController.swift
@@ -11,12 +11,31 @@ import UIKit
 class PokemonDetailVCViewController: UIViewController {
 
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokeIdLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    
+    @IBOutlet weak var evoTextLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    
     
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLabel.text = pokemon.name
     
     }
 
+    @IBAction func backButtonPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
